521103e8d50819b0f64820032c282514f5fd2f11
Carlos Brito <carlos.e1998g@gmail.com>
2023-01-24 14:43:31 -0400

    token y traer usuarios

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,5 +1,8 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const { generateToken } = require("../config/jwtToken");
+
+//REGISTRAR USUARIO
 const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({email:email});
@@ -12,15 +15,33 @@ const createUser = asyncHandler(async (req, res) => {
     }
 });
 
+//LOGIN DE USUARIO
 const loginUserCtrl = asyncHandler(async (req, res) => {
     const {email, password} = req.body;
     // Buscar si existe
     const findUser = await User.findOne({email:email});
     if(findUser && await findUser.isPasswordMatched(password)){
-        res.json(findUser);
+        res.json({
+            id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        });
     } else {
         throw new Error("Credenciales Invalidas");
     }
 });
 
-module.exports = { createUser, loginUserCtrl };
+//TRAER TODOS LOS USUARIOS
+const getallUser = asyncHandler(async (req, res) => {
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers)
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = { createUser, loginUserCtrl, getallUser};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createUser, loginUserCtrl } = require("../controller/userCtrl");
+const { createUser, loginUserCtrl, getallUser } = require("../controller/userCtrl");
 const router = express.Router();
 createUser
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
+router.get("/all-users", getallUser);
 
 module.exports = router;
