d845a57aa63451fdf09714a5c3a1fd0677946181
Carlos Brito <carlos.e1998g@gmail.com>
2023-01-24 10:55:50 -0400

    registro

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const {default: mongoose} = require("mongoose");
 
 const dbConnect = () => {
     try{
-        const conn = mongoose.connect(process.env.MONGODB_URL);
+        const conn = mongoose.connect("mongodb://127.0.0.1:27017/rippleydbecommerce?ssl=false&authSource=admin");
         console.log("Databases connect Successfully");
     } catch (error){
         console.log("Databases error");
