e4f802053efcb767a618604c84fb0cb454491dfd
Carlos Brito <carlos.e1998g@gmail.com>
2023-01-26 15:30:00 -0400

    traer todos los productos y modelo productos 2

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
 
+//CREAR PRODUCTO
 const createProduct = asyncHandler (async (req, res) => {
     try {
         const newProduct = await Product.create(req.body);
@@ -10,12 +11,25 @@ const createProduct = asyncHandler (async (req, res) => {
     }
 });
 
+//TRAER PRODUCTO POR ID
 const getaProduct = asyncHandler (async (req, res) => {
+    const {id} = req.params;
     try {
-        
+        const findProduct = await Product.findById(id);
+        res.json(findProduct);
     } catch (error) {
-        
+        throw new Error(error);
     }
 });
 
-module.exports = {createProduct};
+//TRAER TODOS LOS PRODUCTOS
+const getAllProduct = asyncHandler (async (req, res) => {
+    try {
+        const getallProduct = await Product.find();
+        res.json(getallProduct);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = {createProduct, getaProduct, getAllProduct};
