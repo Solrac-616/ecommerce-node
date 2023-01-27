cc198bbb6e95a2bc9813a55e18a0a8a38f50f412
Carlos Brito <carlos.e1998g@gmail.com>
2023-01-27 13:55:16 -0400

    filtros de productos

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -57,6 +57,7 @@ const getaProduct = asyncHandler (async (req, res) => {
 //TRAER TODOS LOS PRODUCTOS
 const getAllProduct = asyncHandler (async (req, res) => {
     try {
+        // FILTERING
         const queryObj = { ...req.query};
         const excludeFields = ["page", "sort", "limit", "fields"];
         excludeFields.forEach((el) => delete queryObj[el]);
@@ -64,7 +65,35 @@ const getAllProduct = asyncHandler (async (req, res) => {
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-        const query = Product.find(JSON.parse(queryStr));
+        let query = Product.find(JSON.parse(queryStr));
+
+        // SORTING - ORDENAR
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(",").join(" ");
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort("-createdAt");
+        }
+
+        // LIMITANDO CAMPOS
+        if (req.query.fields) {
+            const fields = req.query.fields.split(",").join(" ");
+            query = query.select(fields);
+        } else {
+            query=query.select('-__v');
+        }
+
+        //PAGINACION
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        console.log(page, limit, skip);
+        query = query.skip(skip).limit(limit);
+        if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error("esta pagina no existe");
+        }
+        console.log(page, limit, skip);
 
         const product = await query;
         res.json(product);
