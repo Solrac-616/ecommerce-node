82dc0dee3b170ed611b08fbbb48d4e17b67418f6
Carlos Brito <carlos.e1998g@gmail.com>
2023-01-28 13:17:26 -0400

    blog create and update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
 const cookieParser = require("cookie-parser");
 const morgan  = require("morgan");
 dbConnect();
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api/user/auth", authRouter);
 app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
 
 
 app.use(notFound);
