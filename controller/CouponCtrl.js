ff0afb2adda317d2422dbf5f6f1c038821038219
Carlos Brito <carlos.e1998g@gmail.com>
2023-01-30 18:12:04 -0400

    Actualizar y borrar cupones

--- controller/CouponCtrl.js
+++ controller/CouponCtrl.js
@@ -12,4 +12,40 @@ const createCoupon = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createCoupon };
+//TRAER TODOS LOS CCUPONES
+const getAllCoupons = asyncHandler(async (req, res) => {
+    try {
+        const coupons = await Coupon.find();
+        res.json(coupons);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//ACTUALIZAR CUPONES
+const updateCoupon = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try {
+        const updateCoupon = await Coupon.findByIdAndUpdate(id, req,body, {
+            new: true,
+        });
+        res.json(updateCoupon);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//BORRAR CUPONES
+const deleteCoupon = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try {
+        const deleteCoupon = await Coupon.findByIdAndDelete(id);
+        res.json(deleteCoupon);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = { createCoupon, getAllCoupons, updateCoupon, deleteCoupon};
--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { createCoupon } = require("../controller/CouponCtrl");
+const { createCoupon, getAllCoupons, updateCoupon, deleteCoupon } = require("../controller/CouponCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createCoupon);
+router.get("/", authMiddleware, isAdmin, getAllCoupons);
+router.put("/:id", authMiddleware, isAdmin, updateCoupon);
+router.delete("/:id", authMiddleware, isAdmin, deleteCoupon);
 
 module.exports = router;
